e6d96acff648eb7c1082b5322044a1c23b9352b8
Muratet Eloi <eloi.muratet@student.ecp.fr>
2020-05-14 10:41:22 +0200

    Tweet scheduling is now handled by Heroku

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import tweepy
-import schedule
-import time
 import random
 from datetime import datetime
 
@@ -16,7 +14,6 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
-
 # Connect to API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -25,7 +22,6 @@ api = tweepy.API(auth)
 # Connect to database
 all_non_tweeted_poems = Poem_database.query.filter_by(tweeted=False).all()
 random_poem = random.choice(all_non_tweeted_poems)
-
 
 poem = Poem(
     author=random_poem.author,
@@ -37,24 +33,15 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print(f"Current Time : {current_time}")
 
-
 print(f"Poem {poem.title} \n {poem.content}")
 poem.parse()
 print(f"Parsed poem {poem.title}: \n ")
 print(*poem.parsed_content, sep="\n + ")
 
-
-def job():
+if __name__ == "__main__":
     try:
         poem.tweet(api)
         random_poem.tweeted = True
         db.session.commit()
     except:
         raise ValueError("Something append")
-
-
-schedule.every().day.at("10:00").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(60)
